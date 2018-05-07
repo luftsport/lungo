069aed422afcc83f7a0f86d4cbadc31ea9106694
Einar Huseby <einar.huseby@gmail.com>
2018-05-07 17:19:45 +0200

    Added id lookup to clubs

--- domain/ka_clubs.py
+++ domain/ka_clubs.py
@@ -7,10 +7,15 @@ definition = {
     'item_title': 'ka_clubs',
     'datasource': {'source': 'ka_clubs',
                    },
-
+    'additional_lookup': {
+        'url': 'regex("[\d{1,9}]+")',
+        'field': 'id',
+    },
+    'extra_response_fields': ['id'],
     'versioning': False,
     'resource_methods': ['GET', 'POST'],
     'item_methods': ['GET', 'PATCH'],
+
 
     'schema': _schema
 }
