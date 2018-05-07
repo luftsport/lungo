a3393428b4ac51df2d216f7273615314b8d59bb8
Einar Huseby <einar.huseby@gmail.com>
2018-05-07 17:16:29 +0200

    Updated domain to ka

--- domain/__init__.py
+++ domain/__init__.py
@@ -2,7 +2,7 @@
 import ka_clubs, ka_competence, ka_members
 
 DOMAIN = {
-    "ka_clubs": ka_clubs.definition,
-    "ka_members": ka_members.definition,
+    "ka/clubs": ka_clubs.definition,
+    "ka/members": ka_members.definition,
     #"values/aggregation": aggregation.minmax
 }
