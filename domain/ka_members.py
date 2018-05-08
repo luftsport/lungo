88c229b302422f3510093ddbd4a8c6225f5fcb18
Einar Huseby <einar.huseby@gmail.com>
2018-05-08 11:46:33 +0200

    Removed versioning for ka_members due to keyerror

--- domain/ka_members.py
+++ domain/ka_members.py
@@ -32,7 +32,7 @@ definition = {
         'field': 'Id',
     },
     'extra_response_fields': ['Id'],
-    'versioning': True,
+    'versioning': False,
     'resource_methods': ['GET', 'POST'],
     'item_methods': ['GET', 'PATCH', 'PUT'],
 
