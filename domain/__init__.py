61e0772093ab98aa436894a6e0e721c8b9c72376
Einar Huseby <einar.huseby@gmail.com>
2018-05-09 19:24:59 +0200

    Added ka_orgs domain

--- domain/__init__.py
+++ domain/__init__.py
@@ -1,8 +1,9 @@
  
-import ka_clubs, ka_competence, ka_members
+import ka_clubs, ka_competence, ka_members, ka_orgs
 
 DOMAIN = {
     "ka/clubs": ka_clubs.definition,
     "ka/members": ka_members.definition,
+    "ka/orgs": ka_orgs.definition
     #"values/aggregation": aggregation.minmax
 }
---
+++ domain/ka_orgs.py
@@ -0,0 +1,35 @@
+
+_schema = {
+
+ 'Address': {'type': 'string'},
+ 'BrregOrgNo': {'type': 'string'},
+ 'City': {'type': 'string'},
+ 'County': {'type': 'string'},
+ 'CountyId': {'type': 'integer'},
+ 'Email': {'type': 'string'},
+ 'Id': {'type': 'integer'},
+ 'Name': {'type': 'string'},
+ 'NameDescr': {'type': 'string'},
+ 'OrgType': {'type': 'string'},
+ 'OrgTypeId': {'type': 'integer'},
+ 'Url': {'type': 'string'},
+ 'Zip': {'type': 'string'},
+ '_down': {'type': 'list', 'default': []},
+ '_up': {'type': 'list', 'default': []},
+}
+
+definition = {
+    'item_title': 'ka_orgs',
+    'datasource': {'source': 'ka_orgs',
+                   },
+    'additional_lookup': {
+        'url': 'regex("[\d{1,9}]+")',
+        'field': 'Id',
+    },
+    'extra_response_fields': ['Id'],
+    'versioning': False,
+    'resource_methods': ['GET', 'POST'],
+    'item_methods': ['GET', 'PATCH', 'PUT'],
+
+    'schema': _schema
+}
