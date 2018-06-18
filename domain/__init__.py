d8d611abea2cbbe984484b9b546ca0d46bca7523
Einar Huseby <einar.huseby@gmail.com>
2018-06-18 18:45:12 +0200

    Domains for NIF api

--- domain/__init__.py
+++ domain/__init__.py
@@ -1,9 +1,10 @@
  
-import ka_clubs, ka_competence, ka_members, ka_orgs
+import ka_clubs, ka_competence, ka_members, ka_orgs, organizations
 
 DOMAIN = {
     "ka/clubs": ka_clubs.definition,
     "ka/members": ka_members.definition,
-    "ka/orgs": ka_orgs.definition
+    "ka/orgs": ka_orgs.definition,
+    "organizations": organizations.definition,
     #"values/aggregation": aggregation.minmax
 }
---
+++ domain/organizations.py
@@ -0,0 +1,63 @@
+_schema = {
+
+    'Account': { 'type': 'dict',
+        'schema': {'AccountId': {'type': 'integer'},
+                   'AccountNo': {'type': 'integer'}}
+    },
+    #'ActiveLabel': {'type': 'string'},
+    #'CancellationDate': None,
+    #'CancellationTypeId': None,
+    'Comment': {'type': 'string', 'default': ''},
+    'Contact': {'type': 'dict', 'default': {}},
+    #'ContactId': {'type': 'integer'},
+    'Created': {'type': 'datetime'},
+    'DescribingName': {'type': 'string', 'default': ''},
+    'IsActive': {'type': 'boolean'},
+    'LocalCouncilId': {'type': 'integer'},
+    'LocalCouncilName': {'type': 'string', 'default': ''},
+    'Modified': {'type': 'datetime'},
+    'NIFOrganizationNumber': {'type': 'string', 'default': ''},
+    'Name': {'type': 'string', 'default': ''},
+    'OrgId': {'type': 'integer'},
+    'OrgStructuresDown': {'type': 'list',
+                          'schema': {'type': 'dict', 'schema': {'id': {'type': 'integer'},
+                                                                'type': {'type': 'integer'}}}
+                          },
+    'OrgStructuresUp': {'type': 'list',
+                        'schema': {'type': 'dict', 'schema': {'id': {'type': 'integer'},
+                                                              'type': {'type': 'integer'}}}
+                          },
+
+    'OrganizationTypeId': {'type': 'integer'},
+    'ParentOrganizationId': {'type': 'integer'},
+    'RegisterAuthorityOrganizationNumber': {'type': 'string'},
+    'ShortName': {'type': 'string'},
+    'Activities': {'type': 'list', 'schema': {'type': 'dict', 'schema': {
+        'ActivityCode': {'type': 'string'},
+        'ActivityId': {'type': 'integer'},
+        'Name': {'type': 'string'}
+
+    }}},
+    'MainActivity': { 'type': 'dict',
+                      'schema': {'ActivityCode': {'type': 'string'},
+                                'ActivityId': {'type': 'integer'},
+                                'Name': {'type': 'string'}
+                                },
+    }
+}
+
+definition = {
+    'item_title': 'organizations',
+    'datasource': {'source': 'organizations',
+                   },
+    'additional_lookup': {
+        'url': 'regex("[\d{1,9}]+")',
+        'field': 'OrgId',
+    },
+    'extra_response_fields': ['OrgId'],
+    'versioning': False,
+    'resource_methods': ['GET', 'POST'],
+    'item_methods': ['GET', 'PATCH', 'PUT'],
+
+    'schema': _schema
+}
