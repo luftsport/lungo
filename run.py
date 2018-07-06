fa5308177f10712ace6acc5334263b1d7d22d58d
Einar Huseby <einar.huseby@gmail.com>
2018-07-06 21:13:07 +0200

    Added id to body of redirect

--- run.py
+++ run.py
@@ -87,7 +87,8 @@ def after_get_persons(request, response):
         response.status_code = 301
         response.set_data(json.dumps({'_status': 'ERR',
                                       '_error': '301 Moved permanently',
-                                      '_url': '/api/v1/persons/%s' % d['_merged_to']}))
+                                      '_url': '/api/v1/persons/%s' % d['_merged_to'],
+                                      'id': d['_merged_to']}))
 
 app.on_post_GET_persons += after_get_persons
 """
