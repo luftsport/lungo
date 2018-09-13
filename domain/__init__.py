1f004f88d72ca2e5e13bc35c6546800a68a35fc6
Einar Huseby <einar.huseby@gmail.com>
2018-09-13 17:05:20 +0200

    Added domain to resolve org id to activity id

--- domain/__init__.py
+++ domain/__init__.py
@@ -1,11 +1,12 @@
  
-import ka_clubs, ka_competence, ka_members, ka_orgs, ka_licenses
+import ka_clubs, ka_competence, ka_members, ka_orgs, ka_licenses, ka_org_activity
 import organizations, organization_types, functions, function_types, integration, changes, changes_stream, persons
 
 DOMAIN = {
     "ka/clubs": ka_clubs.definition,
     "ka/members": ka_members.definition,
     "ka/orgs": ka_orgs.definition,
+    "ka/orgs/activity": ka_org_activity.definition,
     "ka/competences": ka_competence.definition,
     "ka/licenses": ka_licenses.definition,
     "organizations": organizations.definition,
@@ -15,6 +16,6 @@ DOMAIN = {
     "integration": integration.definition,
     "integration/changes": changes.definition,
     "integration/changes/stream": changes_stream.definition,
-    "persons": persons.definition
+    "persons": persons.definition,
     #"values/aggregation": aggregation.minmax
 }
---
+++ domain/ka_org_activity.py
@@ -0,0 +1,23 @@
+
+_schema = {
+ 'id': {'type': 'integer', 'unique': True},
+ 'name': {'type': 'string'},
+ 'activity_id': {'type': 'integer'},
+ 'org_type_id': {'type': 'integer'},
+}
+
+definition = {
+    'item_title': 'ka_org_activity',
+    'datasource': {'source': 'ka_org_activity',
+                   },
+    'additional_lookup': {
+        'url': 'regex("[\d{1,9}]+")',
+        'field': 'id',
+    },
+    'extra_response_fields': ['id'],
+    'versioning': False,
+    'resource_methods': ['GET', 'POST'],
+    'item_methods': ['GET', 'PATCH', 'PUT'],
+
+    'schema': _schema
+}
