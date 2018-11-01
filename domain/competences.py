90c3db58ae87526ba83bc4db86e21e1009e12c7b
Einar Huseby <einar.huseby@gmail.com>
2018-11-01 14:46:51 +0100

    Domain updated to reflect new licenses and competences

--- domain/competences.py
+++ domain/competences.py
@@ -1,32 +1,28 @@
-
 _schema = {
 
- 'Address': {'type': 'string'},
- 'BrregOrgNo': {'type': 'string'},
- 'City': {'type': 'string'},
- 'County': {'type': 'string'},
- 'CountyId': {'type': 'integer'},
- 'Email': {'type': 'string'},
- 'Id': {'type': 'integer', 'unique': True},
- 'Name': {'type': 'string'},
- 'NameDescr': {'type': 'string'},
- 'OrgType': {'type': 'string'},
- 'OrgTypeId': {'type': 'integer'},
- 'Url': {'type': 'string'},
- 'Zip': {'type': 'string'},
- '_down': {'type': 'list', 'default': []},
- '_up': {'type': 'list', 'default': []},
+    'approved_by_org_id': {'type': 'integer'},
+    'course_id': {'type': 'integer'},
+    'passed': {'type': 'boolean'},
+    'person_id': {'type': 'integer'},
+    'id': {'type': 'integer', 'required': True, 'unique': True},
+    'sald': {'type': 'integer'},
+    'title': {'type': 'string'},
+    'type_id': {'type': 'integer'},
+    'additional_title': {'type': 'string'},
+    '_section': {'type': 'string'},
+    '_code': {'type': 'string'},
+    '_name': {'type': 'string'}
 }
 
 definition = {
-    'item_title': 'ka_orgs',
-    'datasource': {'source': 'ka_orgs',
+    'item_title': 'Competences',
+    'datasource': {'source': 'competences',
                    },
     'additional_lookup': {
         'url': 'regex("[\d{1,9}]+")',
-        'field': 'Id',
+        'field': 'id',
     },
-    'extra_response_fields': ['Id'],
+    'extra_response_fields': ['id'],
     'versioning': False,
     'resource_methods': ['GET', 'POST'],
     'item_methods': ['GET', 'PATCH', 'PUT'],
