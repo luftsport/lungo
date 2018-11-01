513a34c904753d6f22c13d70eef70a0138bd8112
Einar Huseby <einar.huseby@gmail.com>
2018-11-01 15:04:43 +0100

    Fix cut error

--- domain/integration_users.py
+++ domain/integration_users.py
@@ -1,25 +1,5 @@
 from bson import SON
 
-_schema = {
-
-    'active_clubs': {'type': 'list'},
-    'active_functions': {'type': 'list'},
-    'functions': {'type': 'list'},
-    'clubs': {'type': 'list'},
-    'club_id': {'type': 'integer', 'required': True, 'unique': True},
-    'club_created': {'type': 'datetime'},
-    'id': {'type': 'integer', 'required': True},
-    'first_name': {'type': 'string'},
-    'full_name': {'type': 'string'},
-    'login': {'type': 'string'},
-    'password': {'type': 'string'},
-    'person_id': {'type': 'integer'},
-    'user_id': {'type': 'integer'},
-    'username': {'type': 'string'},
-    'modified': {'type': 'datetime'}
-    
-}
-"""
 _schema = {'username': {'type': 'string', 'required': True},
            'password': {'type': 'string', 'required': True},
            'id': {'type': 'integer', 'required': True, 'unique': True},
