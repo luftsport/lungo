ddf4b0eb63bc2c76c9c49fb7ca49ac5b3e9db0c0
Einar Huseby <einar.huseby@gmail.com>
2018-11-01 14:49:42 +0100

    Remove function_* prefix in typings

--- domain/function_types.py
+++ domain/function_types.py
@@ -1,9 +1,9 @@
 _schema = {
     'description': {'type': 'string'},
-    'function_category_id': {'type': 'integer'},
-    'function_category_name': {'type': 'string'},
-    'function_type_id': {'type': 'integer', 'unique': True},
-    'function_type_no': {'type': 'string'},
+    'category_id': {'type': 'integer'},
+    'category_name': {'type': 'string'},
+    'type_id': {'type': 'integer', 'unique': True},
+    'type_no': {'type': 'string'},
     'is_license': {'type': 'boolean'},
     'is_valid': {'type': 'boolean'},
     'name': {'type': 'string'},
@@ -15,13 +15,13 @@ _schema = {
 
 definition = {
     'item_title': 'function types',
-    'datasource': {'source': 'function_types',
+    'datasource': {'source': 'types',
                    },
     'additional_lookup': {
         'url': 'regex("[\d{1,9}]+")',
-        'field': 'function_type_id',
+        'field': 'type_id',
     },
-    'extra_response_fields': ['function_type_id'],
+    'extra_response_fields': ['type_id'],
     'versioning': False,
     'resource_methods': ['GET', 'POST', 'DELETE'],
     'item_methods': ['GET', 'PATCH', 'PUT'],
