095ded39bbf24028aa4393d01aed3d111070f1d6
Einar Huseby <einar.huseby@gmail.com>
2018-11-11 23:27:12 +0100

    Resouce name from - to _

--- domain/__init__.py
+++ domain/__init__.py
@@ -14,50 +14,50 @@ DOMAIN = {
     # testref": test_ref.definition,
 
     # KA
-    "ka-clubs": ka_clubs.definition,
-    "ka-members": ka_members.definition,
-    "ka-orgs": ka_orgs.definition,
-    "ka-orgs-activity": ka_org_activity.definition,
-    "ka-competences": ka_competence.definition,
-    "ka-licenses": ka_licenses.definition,
+    "ka_clubs": ka_clubs.definition,
+    "ka_members": ka_members.definition,
+    "ka_orgs": ka_orgs.definition,
+    "ka_orgs_activity": ka_org_activity.definition,
+    "ka_competences": ka_competence.definition,
+    "ka_licenses": ka_licenses.definition,
 
     # Persons
     "persons": persons.definition,
-    "persons-process": persons.process_definition,
-    "persons-search": persons.search_definition,
-    "persons-gender": persons.agg_count_gender,
+    "persons_process": persons.process_definition,
+    "persons_search": persons.search_definition,
+    "persons_gender": persons.agg_count_gender,
 
     # Organization
     "organizations": organizations.definition,
-    "organizations-process": organizations.process_definition,
-    "organizations-types": organization_types.definition,
-    "organizations-types-count": organizations.agg_count_types,
+    "organizations_process": organizations.process_definition,
+    "organizations_types": organization_types.definition,
+    "organizations_types_count": organizations.agg_count_types,
 
     # Functions
     "functions": functions.definition,
-    "functions-process": functions.process_definition,
-    "functions-types-count": functions.agg_count_types,
-    "functions-types": function_types.definition,
+    "functions_process": functions.process_definition,
+    "functions_types_count": functions.agg_count_types,
+    "functions_types": function_types.definition,
 
     # Competences
     "competences": competences.definition,
-    "competences-process": competences.process_definition,
-    "competences-types": competences_types.definition,
-    "competences-codes": competences.agg_count_codes,
+    "competences_process": competences.process_definition,
+    "competences_types": competences_types.definition,
+    "competences_codes": competences.agg_count_codes,
 
     # Licenses
     "licenses": licenses.definition,
-    "licenses-process": licenses.process_definition,
-    "licenses-types": licenses_types.definition,
+    "licenses_process": licenses.process_definition,
+    "licenses_types": licenses_types.definition,
 
     # Integration
-    "integration-users": integration_users.definition,
-    "integration-users-clubs": integration_users.agg_count_clubs,
-    "integration-changes": integration_changes.definition,
-    "integration-changes-entity-types": integration_changes.agg_count_entity_types,
-    "integration-changes-clubs": integration_changes.agg_count_clubs,
-    "integration-changes-status": integration_changes.agg_count_statuses,
-    "integration-changes-change-types": integration_changes.agg_count_change_types,
+    "integration_users": integration_users.definition,
+    "integration_users_clubs": integration_users.agg_count_clubs,
+    "integration_changes": integration_changes.definition,
+    "integration_changes_entity_types": integration_changes.agg_count_entity_types,
+    "integration_changes_clubs": integration_changes.agg_count_clubs,
+    "integration_changes_status": integration_changes.agg_count_statuses,
+    "integration_changes_change_types": integration_changes.agg_count_change_types,
 
-    # "values-aggregation": aggregation.minmax
+    # "values_aggregation": aggregation.minmax
 }
