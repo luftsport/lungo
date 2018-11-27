0fe2e9bbe66b37f1c3b4cb913e034e3af36f7531
Einar Huseby <einar.huseby@gmail.com>
2018-11-27 15:13:04 +0100

    Redone hooks to possibly speed up by local comp before/if patch

--- ext/app/hooks.py
+++ ext/app/hooks.py
@@ -3,15 +3,22 @@ To hook all the different changes to our api!
 """
 from eve.methods.patch import patch_internal
 from eve.methods.get import get_internal
-import geocoder
-import dateutil.parser
 from datetime import datetime
+from operator import itemgetter
+
+# import dateutil.parser
+
 
 RESOURCE_PERSONS_PROCESS = 'persons_process'
 
 
 def _get_end_of_year():
     return datetime(datetime.utcnow().year, 12, 31, 23, 59, 59, 999999)
+
+
+def _get_end_of_january():
+    """End of jan next year"""
+    return datetime(datetime.utcnow().year + 1, 1, 31, 23, 59, 59, 999999)
 
 
 def _get_person(person_id) -> dict:
@@ -29,6 +36,27 @@ def _get_person(person_id) -> dict:
             return person['_items'][0]
 
     return {}
+
+
+def _compare_list_of_dicts(l1, l2, dict_id='id') -> bool:
+    """Sorts lists then compares on the given id in the dicts
+    :param l1: list of dicts
+    :type l1: list(dict)
+    :param l2: list of dicts
+    :type l2: list(dict)
+    :param dict_id: The id for the dicts
+    :type dict_id: any
+    :return: True if difference, False if not or can't decide
+    """
+    try:
+        list_1, list_2 = [sorted(l, key=itemgetter(dict_id)) for l in (l1, l2)]
+        pairs = zip(list_1, list_2)
+        if any(x != y for x, y in pairs):
+            return True
+        else:
+            return False  # They are equal
+    except:
+        return True  # We do not know if difference
 
 
 def _get_org(org_id) -> dict:
@@ -49,6 +77,24 @@ def _get_org(org_id) -> dict:
     return {}
 
 
+def _get_functions_types(type_id) -> dict:
+    """Get org from organizations internal
+
+    :param org_id: Organization id
+    :type org_id: int
+    :return org: Returns the organization
+    :rtype: dict
+    """
+
+    function_type, _, _, status, _ = get_internal('functions_types', **{'id': type_id})
+
+    if status == 200:
+        if '_items' in function_type and len(function_type['_items']) == 1:
+            return function_type['_items'][0]
+
+    return {}
+
+
 def on_function_post(items) -> None:
     """On every function change, let's update person's functions.
 
@@ -65,7 +111,11 @@ def on_function_post(items) -> None:
         on_function_put(response)
 
 
-def on_function_put(response):
+def on_function_put(response) -> None:
+    """
+    :param response: database object
+    :return: None
+    """
     person = _get_person(response['person_id'])
 
     if '_id' in person:
@@ -76,12 +126,14 @@ def on_function_put(response):
 
         # Expiry date
         expiry = response.get('to_date', None)
-        # Set expiry to end year
-        if expiry is None:
-            expiry = _get_end_of_year()
 
-        if response.get('type_id', None) == 10000000:
+        if response.get('type_id', 0) == 10000000:
 
+            # Set expiry to end year
+            if expiry is None:
+                expiry = _get_end_of_january()
+
+            # If not deleted and is valid expiry add to club list
             if not response['is_deleted'] and not response['is_passive'] and \
                             expiry is not None and expiry > datetime.utcnow():
 
@@ -99,27 +151,36 @@ def on_function_put(response):
             # Valid expiry?
             # clubs[:] = [d for d in clubs if d.get('expiry') >= datetime.utcnow()]
 
-            # Activities!
+
+            # Activities
+            # Do not know which club is actually which activity
+            # Need to redo all.
             for club_id in clubs:
                 try:
                     org = _get_org(club_id)
-                    activity = org.get('main_activity', None)
+                    # Gets the id of main activity
+                    # If None, go for 27 (Luftsport/370)
+                    # @TODO see if should be None to pass next
+                    activity = org.get('main_activity', {}).get('id', 27)
                     if activity is not None:
                         # @TODO see if code should be integer? String now.
-                        activity['code'] = int(activity.get('code', 0))
+                        # activity['code'] = int(activity.get('code', 0))
                         activities.append(activity)
                 except:
                     pass
 
             # Unique list of activities
-            activities = list({v['id']: v for v in activities}.values())
+            activities = list(set(activities))
+
+            # List of dicts
+            # activities = list({v['id']: v for v in activities}.values())
             # Valid expiry?
             # activities[:] = [d for d in activities if d.get('expiry') >= datetime.utcnow()]
 
         # The rest of the functions
-        # Also exoiry date!
+        # Considers expiry date, if None then still valid
         f = person.get('functions', [])
-        if expiry > datetime.utcnow():
+        if expiry is None or expiry > datetime.utcnow():
             f.append(response['id'])
         else:
             try:
@@ -133,13 +194,20 @@ def on_function_put(response):
 
         lookup = {'_id': person['_id']}
 
+        # Update person with new values
         # response, last_modified, etag, status =
         patch_internal(RESOURCE_PERSONS_PROCESS,
-                       {'functions': f, 'activities': activities,
-                        'clubs': clubs},
+                       {'functions': f, 'activities': activities, 'clubs': clubs},
                        False, True, **lookup)
-        # print(response, status)
-        # patch_internal(RESOURCE_PERSONS_PROCESS, {'competences': l}, False, True, **look)
+
+    # Always check and get type name
+    # Update the function
+    if response.get('type_name', None) is None:
+        function_type = _get_functions_types(response.get('type_id', 0))
+        if len(function_type) > 0:
+            type_name = function_type.get('name', None)
+            if type_name is not None:
+                patch_internal('functions', {'type_name': type_name}, False, True, **{'_id': response.get('_id')})
 
 
 def on_license_post(items):
@@ -153,16 +221,20 @@ def on_license_put(response):
     """pass"""
 
     expiry = response.get('period_to_date', None)  # dateutil.parser.parse(response.get('period_to_date', None))
+
     # Set expiry to end year
     if expiry is None:
         expiry = _get_end_of_year()
 
-    if expiry is None or expiry >= datetime.utcnow():
-        person = _get_person(response['person_id'])
+    # Always get person
+    person = _get_person(response['person_id'])
+    if '_id' in person:
 
-        if '_id' in person:
+        licenses = person.get('licenses', [])
 
-            licenses = person.get('licenses')
+        # If valid expiry
+        if expiry is None or expiry >= datetime.utcnow():
+
             try:
                 licenses.append({'id': response.get('id'),
                                  'status_id': response.get('status_id', 0),
@@ -171,13 +243,17 @@ def on_license_put(response):
                                  'type_id': response.get('type_id', None),
                                  'type_name': response.get('type_name', None)})
 
-                # Unique
-                licenses = list({v['id']: v for v in licenses}.values())
-                # Valid expiry
-                licenses[:] = [d for d in licenses if d.get('expiry') >= datetime.utcnow()]
             except:
                 pass
 
+        # Unique
+        licenses = list({v['id']: v for v in licenses}.values())
+
+        # Valid expiry
+        licenses[:] = [d for d in licenses if d.get('expiry') >= datetime.utcnow()]
+
+        # Patch if difference
+        if '_id' in person and _compare_list_of_dicts(licenses, person.get('licenses', [])) is True:
             lookup = {'_id': person['_id']}
             patch_internal(RESOURCE_PERSONS_PROCESS, {'licenses': licenses}, False, True, **lookup)
 
@@ -195,28 +271,21 @@ def on_competence_post(items):
 
 def on_competence_put(response):
     """"""
-    # try:
-    #    expiry = dateutil.parser.parse(response.get('valid_until', None))
-    # except:
-    #    expiry = None
+
     expiry = response.get('valid_until', None)
 
     # Set expiry to end year
     if expiry is None:
         expiry = _get_end_of_year()
 
-    if expiry is not None and isinstance(expiry, datetime) and expiry >= datetime.utcnow():
+    person = _get_person(response['person_id'])
 
-        person = _get_person(response['person_id'])
+    if '_id' in person:
 
-        if '_id' in person:
-            competence = person.get('competences', [])
+        competence = person.get('competences', [])
 
-            # Remove stale competences?
-            try:
-                competence[:] = [d for d in competence if d.get('expiry') >= datetime.utcnow()]
-            except:
-                pass
+        # Add this competence?
+        if expiry is not None and isinstance(expiry, datetime) and expiry >= datetime.utcnow():
 
             try:
                 competence.append({'id': response.get('id'),
@@ -224,13 +293,17 @@ def on_competence_put(response):
                                    'issuer': response.get('approved_by_person_id', None),
                                    'expiry': response.get('valid_until', None),
                                    'paid': response.get('paid_date', None)})
-
-                competence = list({v['id']: v for v in competence}.values())
-
-
             except:
                 pass
 
+        # Always remove stale competences
+        competence[:] = [d for d in competence if d.get('expiry') >= datetime.utcnow()]
+
+        # Always unique by id
+        competence = list({v['id']: v for v in competence}.values())
+
+        # Patch if difference
+        if _compare_list_of_dicts(competence, person.get('competence', [])) is True:
             lookup = {'_id': person['_id']}
             patch_internal(RESOURCE_PERSONS_PROCESS, {'competences': competence}, False, True, **lookup)
 
