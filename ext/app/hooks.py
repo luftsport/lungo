5af6d28a8758efd2e658eaa809552a5ed7a4848c
Einar Huseby <einar.huseby@gmail.com>
2018-12-29 19:46:41 +0100

    Supplementing with app.logger

--- ext/app/hooks.py
+++ ext/app/hooks.py
@@ -262,6 +262,7 @@ def on_license_post(items):
 def on_license_put(response, original=None):
     """pass"""
     print(response)
+    app.logger.info('In put license')
 
     expiry = response.get('period_to_date', None)  # dateutil.parser.parse(response.get('period_to_date', None))
 
