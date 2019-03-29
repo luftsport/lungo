d68a692605673a3a672fc2e459ec111836453739
Einar Huseby <einar.huseby@gmail.com>
2019-03-29 13:31:01 +0100

    org_id and org_type_id

--- domain/functions.py
+++ domain/functions.py
@@ -10,6 +10,15 @@ _schema = {
 
                          },
                          },
+    'org_id': {'type': 'integer',
+               'data_relation': {
+                   'resource': 'organizations',
+                   'field': 'id',
+                   'embeddable': True,
+
+               },
+               },
+    'org_type_id': {'type': 'integer'},
     'active_in_org_name': {'type': 'string'},
     'contact_id': {'type': 'integer'},
     'contact_information_id': {'type': 'integer'},
@@ -54,7 +63,8 @@ definition = {
                       'person_id': ([('person_id', 1)], {'background': True}),
                       'type_id': ([('type_id', 1)], {'background': True}),
                       'to_date': ([('to_date', 1)], {'background': True}),
-                      'active_in_org_id': ([('active_in_org_id', 1)], {'background': True}),
+                      'active_in_org_id': (
+                      [('active_in_org_id', 1), ('org_id', 1), ('org_type_id', 1)], {'background': True}),
                       'has_paid_membership': ([('has_paid_membership', 1)], {'background': True}),
                       'type_name': ([('type_name', 'text')], {'background': True})
                       },
@@ -64,6 +74,7 @@ definition = {
 # Process resource without data_relations
 _schema_process = _schema.copy()
 _schema_process['active_in_org_id'] = {'type': 'integer'}
+_schema_process['org_id'] = {'type': 'integer'}
 _schema_process['person_id'] = {'type': 'integer'}
 
 process_definition = {
