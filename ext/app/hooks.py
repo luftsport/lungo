1ba3884d2211756096a98eabb34eb0dc27f9137b
Einar <einar.huseby@gmail.com>
2020-05-08 18:29:15 +0200

    Removed debug prints

--- ext/app/hooks.py
+++ ext/app/hooks.py
@@ -832,9 +832,6 @@ def on_person_after_post(items):
 
 
 def on_person_before_put(item, original):
-    print('BEFORE')
-    print('ITEM', item)
-    print('ORIG', original)
     # if original then use and not rebuild because
     # functions, competences, licenses, memberships and clubs, activities
     item['functions'] = original.get('functions', [])
@@ -855,9 +852,6 @@ def on_person_before_put(item, original):
 
 def on_person_after_put(item, original=None):
     # _update_person(item)
-    print('AFTER')
-    print('ITEM', item)
-    print('ORIG', original)
     try:
         broadcast({'entity': 'person',
                    'entity_id': item['id'],
