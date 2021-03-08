7896882aa7d8e7a13d9720204cdd2b328042779a
Einar <einar.huseby@gmail.com>
2021-03-08 20:13:16 +0100

    Seperate version file with history

---
+++ version.py
@@ -0,0 +1,11 @@
+__version_info__ = ('1', '0', '4')
+
+"""
+VERSION HISTORY
+
+1.0.4 2021-03-08: 
+- Added support for merged persons' payments
+- Do not process competences without expiry date
+
+
+"""
