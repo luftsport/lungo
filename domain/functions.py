d7112bd907f6f8780ee5932a0afcb6a141a365c1
Einar <einar.huseby@gmail.com>
2022-04-28 11:33:19 +0200

    Filter for active functions

--- domain/functions.py
+++ domain/functions.py
@@ -50,6 +50,11 @@ definition = {
     'url': 'functions',
     'item_title': 'functions',
     'datasource': {'source': RESOURCE_COLLECTION,
+                   'filter': {
+                       # Show active by default
+                       'is_passive': False,
+                       'is_deleted': False
+                   }
                    },
     'additional_lookup': {
         'url': 'regex("[\d{1,9}]+")',
