4a9e4c4f927dc1872859e2d3169eec82eb79bd82
Einar <einar.huseby@gmail.com>
2022-04-28 12:54:01 +0200

    removed static filter moved to clients lookup

--- domain/functions.py
+++ domain/functions.py
@@ -50,12 +50,12 @@ definition = {
     'url': 'functions',
     'item_title': 'functions',
     'datasource': {'source': RESOURCE_COLLECTION,
-                   'filter': {
+                   #'filter': {
                        # Show only active by default
                        # @TODO move to hook and add filter if not set
-                       'is_passive': False,
-                       'is_deleted': False
-                   }
+                   #    'is_passive': False,
+                   #    'is_deleted': False
+                   #}
                    },
     'additional_lookup': {
         'url': 'regex("[\d{1,9}]+")',
