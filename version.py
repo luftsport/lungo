2f1c7595a877e77ae099554872a40ec21a75c54c
Einar <einar.huseby@gmail.com>
2023-10-21 12:40:49 +0200

    Version bump

--- version.py
+++ version.py
@@ -1,7 +1,10 @@
-__version_info__ = ('1', '0', '4')
+__version_info__ = ('1', '0', '5')
 
 """
 VERSION HISTORY
+
+1.0.5
+- Correcting errors done with 1.0.3 vs 1.0.4 and hotfix vs feature branches
 
 1.0.4
 - Added support for merged persons' payments
