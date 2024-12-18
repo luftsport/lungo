10762c17d7d395d7c9e1c1521e1bf2a9808ac4f1
Einar <einar.huseby@gmail.com>
2024-12-18 19:05:04 +0100

    UTC snaikoil

--- ext/app/helpers.py
+++ ext/app/helpers.py
@@ -29,7 +29,8 @@ def _fix_naive(date_time):
     if isinstance(date_time, datetime):
         if date_time.tzinfo is None or date_time.tzinfo.utcoffset(date_time) is None:
             """self.org_created is naive, no timezone we assume UTC"""
-            date_time = date_time.replace(tzinfo=tz_local)
+            # date_time = date_time.replace(tzinfo=tz_local)
+            date_time = (date_time.replace(tzinfo=None) - date_time.utcoffset()).replace(tzinfo=tz_utc)
 
     return date_time
 
