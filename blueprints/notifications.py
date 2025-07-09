eaf9c43202db36a405ac6a3bc4a1f3111dab1f60
Einar <einar.huseby@gmail.com>
2025-07-09 15:42:21 +0200

    Support wildcard for both org and activity

--- blueprints/notifications.py
+++ blueprints/notifications.py
@@ -128,8 +128,9 @@ def get_users_from_role(role):
 
     # role = {key: int(value) for key, value in role.items()}
     if ('role' and 'org' and 'activity') in role:
-
-        if role['org'] is not None and role['org'] != '*' and role['org'] > 0:
+        if role['org'] == '*' and role['activity'] == '*':
+            query = f'where={{ "type_id": {role["role"]}, "is_deleted": false, "is_passive": false}}&projection={{"person_id": 1}}'
+        elif role['org'] is not None and role['org'] != '*' and role['org'] > 0:
             query = f'where={{"org_id": {role["org"]}, "type_id": {role["role"]}, "is_deleted": false, "is_passive": false}}&projection={{"person_id": 1}}'
         elif role['org'] == '*' and role['activity'] is not None and role['activity'] != '*' and role['activity'] > 0:
             orgs_from_activity = get_orgs_in_activivity(role['activity'])
