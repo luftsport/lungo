64373e34bacb3ea6bc6228efa720be5c600a8649
Einar <einar.huseby@gmail.com>
2026-03-02 16:30:03 +0100

    Upping the logfile sizes

--- run.py
+++ run.py
@@ -193,7 +193,7 @@ if 1 == 1 or not app.debug:
     import logging
     from logging.handlers import RotatingFileHandler
 
-    file_handler = RotatingFileHandler('lungo-backend.log', 'a', 1 * 1024 * 1024, 10)
+    file_handler = RotatingFileHandler('lungo-backend.log', 'a', 25 * 1024 * 1024, 10)
     file_handler.setFormatter(
         logging.Formatter('%(asctime)s %(levelname)s: %(message)s [in %(pathname)s:%(lineno)d]'))
     app.logger.setLevel(logging.DEBUG)
